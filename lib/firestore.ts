f9045699cff5639ef6b254940650862142a0c4f9
mym-dev-ui <saddam.saqqar@icloud.com>
2026-06-18 22:53:07 +0300

    Update firestore.ts

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACyKItfyP7-ebn2n6vKIl11S7P_TKPhko",
-  authDomain: "cares-main-m.firebaseapp.com",
-  databaseURL: "https://cares-main-m-default-rtdb.firebaseio.com",
-  projectId: "cares-main-m",
-  storageBucket: "cares-main-m.firebasestorage.app",
-  messagingSenderId: "777492774598",
-  appId: "1:777492774598:web:855835b3d8c79a589f21c7",
-  measurementId: "G-HLJC35394P"
+  apiKey: "AIzaSyBzftpB6LUJUBNvQ12y979F8GM4NiZKItY",
+  authDomain: "zzaassff-lll.firebaseapp.com",
+  databaseURL: "https://zzaassff-lll-default-rtdb.firebaseio.com",
+  projectId: "zzaassff-lll",
+  storageBucket: "zzaassff-lll.firebasestorage.app",
+  messagingSenderId: "1097233727284",
+  appId: "1:1097233727284:web:2a8c238e7ca54dfcec2f61",
+  measurementId: "G-CZNF1LL514"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
