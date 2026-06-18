58e4c655bbb8f968df6fd954e610c2ba06fb241b
mym-dev-ui <saddam.saqqar@icloud.com>
2026-06-18 22:52:35 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,8 @@ export default function LoginPage() {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState("")
   const [formData, setFormData] = useState<LoginFormData>({
-    email: "",
-    password: "",
+    email: "sadda@gmail.com",
+    password: "sadda@gmail.com",
   })
   const router = useRouter()
 
